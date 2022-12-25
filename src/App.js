93aa3d0b74773e86a6e591cca9b921c5e265dadc
Fahrad <79330682+Fahrad20@users.noreply.github.com>
2022-12-25 22:56:02 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+    return (
+        <main>
+            <div className="left">
+                <div className="img"/>
+                <div className="info">
+                    <h2>Fahrad levonyan</h2>
+                    <h3>Front-end Developer</h3>
+                    <p>I know the required front-end tools, such as: React.js, Next.js and Typescript.
+                        I know advanced JavaScript.
+                        Excellent knowledge of HTML and CSS.</p>
+                </div>
+                <div className="social">
+                    <a href="https://www.linkedin.com/in/levonyanfahrad" target='_blank'/>
+                    <a href="https://github.com/Fahrad20" target='_blank'/>
+                    <a href="https://www.facebook.com/levonyanfahrad20" target='_blank'/>
+                    <a href="Fahrad Levonyan(CV).pdf" download/>
+                </div>
+            </div>
+            <div className="portfolio">
+                <h3>Portfolio</h3>
+                <a href='https://cybertech.am' className='item' target='_blank'/>
+                <a href='https://erebuni.am' className='item' target='_blank'/>
+                <a href='https://zambi.am' className='item' target='_blank'/>
+            </div>
+        </main>
+    );
+}
+
+export default App;
